9e19ad5823efc4a0ab30487329bab578c9df309d
ehrom <19820528+EhRom@users.noreply.github.com>
2026-04-10 18:44:50 +0200

    Add AzMaps tests

---
+++ Tests.Puffix.Rest/Infra/AzMaps/AzMapsApiHttpRepository.cs
@@ -0,0 +1,30 @@
+﻿using Puffix.Rest;
+
+namespace Tests.Puffix.Rest.Infra.AzMaps;
+
+public class AzMapsApiHttpRepository(IHttpClientFactory httpClientFactory) :
+    RestHttpRepository<IAzMapsApiQueryInformation, IAzMapsApiToken>(httpClientFactory),
+    IAzMapsApiHttpRepository
+{
+    public override IAzMapsApiQueryInformation BuildAuthenticatedQuery(IAzMapsApiToken token, HttpMethod httpMethod, string apiUri, string queryPath, IDictionary<string, string> queryParameters, IQueryContent queryContent)
+    {
+        IDictionary<string, IEnumerable<string>> headers = new Dictionary<string, IEnumerable<string>>();
+        return AzMapsApiQueryInformation.CreateNewAuthenticatedQuery(token, httpMethod, headers, apiUri, queryPath, queryParameters, queryContent);
+    }
+
+    public override IAzMapsApiQueryInformation BuildAuthenticatedQuery(IAzMapsApiToken token, HttpMethod httpMethod, IDictionary<string, IEnumerable<string>> headers, string apiUri, string queryPath, IDictionary<string, string> queryParameters, IQueryContent queryContent)
+    {
+        return AzMapsApiQueryInformation.CreateNewAuthenticatedQuery(token, httpMethod, headers, apiUri, queryPath, queryParameters, queryContent);
+    }
+
+    public override IAzMapsApiQueryInformation BuildUnauthenticatedQuery(HttpMethod httpMethod, string apiUri, string queryPath, IDictionary<string, string> queryParameters, IQueryContent queryContent)
+    {
+        IDictionary<string, IEnumerable<string>> headers = new Dictionary<string, IEnumerable<string>>();
+        return AzMapsApiQueryInformation.CreateNewUnauthenticatedQuery(httpMethod, headers, apiUri, queryPath, queryParameters, queryContent);
+    }
+
+    public override IAzMapsApiQueryInformation BuildUnauthenticatedQuery(HttpMethod httpMethod, IDictionary<string, IEnumerable<string>> headers, string apiUri, string queryPath, IDictionary<string, string> queryParameters, IQueryContent queryContent)
+    {
+        return AzMapsApiQueryInformation.CreateNewUnauthenticatedQuery(httpMethod, headers, apiUri, queryPath, queryParameters, queryContent);
+    }
+}
---
+++ Tests.Puffix.Rest/Main/AzMapsApiTests.cs
@@ -0,0 +1,188 @@
+﻿using Microsoft.Extensions.Configuration;
+using Moq;
+using Moq.Protected;
+using Puffix.IoC;
+using Puffix.IoC.Configuration;
+using Puffix.Rest;
+using System.Net;
+using System.Text;
+using Tests.Puffix.Rest.Infra;
+using Tests.Puffix.Rest.Infra.AzMaps;
+
+namespace Tests.Puffix.Rest.Main;
+
+public class AzMapsApiTests
+{
+
+    private IIoCContainerWithConfiguration container;
+
+    [SetUp]
+    public void Setup()
+    {
+        IConfigurationRoot configuration = new ConfigurationBuilder()
+            .SetBasePath(Directory.GetCurrentDirectory())
+            .AddJsonFile("appSettings.json", optional: false, reloadOnChange: true)
+            .AddJsonFile("appSettings.local.json", optional: true, reloadOnChange: true)
+            .Build();
+
+        container = IoCContainer.CreateNew(configuration);
+    }
+
+    //[Test] // > Sample method, only for local use
+    public async Task Test()
+    {
+        const string azMapsBaseUri = "https://atlas.microsoft.com";
+        const string azMapsSearchAddressQeuryPath = $"search/address/json";
+        try
+        {
+            IAzMapsApiToken token = container.Resolve<IAzMapsApiToken>();
+            IAzMapsApiHttpRepository httpRepository = container.Resolve<IAzMapsApiHttpRepository>();
+
+            IDictionary<string, string> queryParameters = new Dictionary<string, string>()
+            {
+                { "api-version", "1.0" },
+                { "language", "language=en-US" },
+                { "query", "Villeurbanne" }
+            };
+            IQueryContent queryContent = EmptyContent.CreateNew();
+
+            IAzMapsApiQueryInformation queryInformation = httpRepository.BuildAuthenticatedQuery(token, HttpMethod.Get, azMapsBaseUri, azMapsSearchAddressQeuryPath, queryParameters, queryContent);
+
+            string actualResult = await httpRepository.HttpAsync(queryInformation);
+
+            Assert.That(actualResult, Is.Not.Null);
+            Assert.That(actualResult, Is.Not.Empty);
+        }
+        catch (Exception error)
+        {
+            Assert.Fail($"Error while testing {nameof(Test)}: {error.Message}");
+        }
+    }
+
+    [Test]
+    public async Task UnitTest()
+    {
+        const string azMapsBaseUri = "https://atlas.microsoft.com";
+        const string azMapsSearchAddressQueryPath = $"search/address/json";
+
+        try
+        {
+            IDictionary<string, string> queryParameters = new Dictionary<string, string>()
+            {
+                { "api-version", "1.0" },
+                { "language", "language=en-US" },
+                { "query", "Villeurbanne" }
+            };
+            string expectedQueryParameters = BuildExpectedQueryParameters(queryParameters);
+
+            BuildMocks(container, out IAzMapsApiToken token, out Mock<IHttpClientFactory> httpClientFactoryMock, out IAzMapsApiHttpRepository httpRepository);
+
+            // Register HTTP Calls
+            using HttpContent expectedHttpContent = new StringContent(sampleResponse ?? string.Empty, Encoding.UTF8, "application/json");
+
+            Mock<HttpMessageHandler> mockHttpMessageHandlerMock = new Mock<HttpMessageHandler>();
+            mockHttpMessageHandlerMock.Protected()
+                .Setup<Task<HttpResponseMessage>>("SendAsync", ItExpr.IsAny<HttpRequestMessage>(), ItExpr.IsAny<CancellationToken>())
+                .ReturnsAsync(new HttpResponseMessage
+                {
+                    StatusCode = HttpStatusCode.OK,
+                    Content = expectedHttpContent
+                });
+             
+            using HttpClient httpClient = new HttpClient(mockHttpMessageHandlerMock.Object);
+
+            httpClientFactoryMock.Setup(httpClientFactory => httpClientFactory.CreateClient(It.IsAny<string>()))
+                .Returns(httpClient);
+
+
+            // Test
+            IQueryContent queryContent = EmptyContent.CreateNew();
+            IAzMapsApiQueryInformation queryInformation = httpRepository.BuildAuthenticatedQuery(token, HttpMethod.Get, azMapsBaseUri, azMapsSearchAddressQueryPath, queryParameters, queryContent);
+            string actualResult = await httpRepository.HttpAsync(queryInformation);
+
+            // Check calls
+            httpClientFactoryMock.Verify(httpClientFactory => httpClientFactory.CreateClient(It.IsAny<string>()), Times.Once());
+            mockHttpMessageHandlerMock.Protected().Verify(
+                "SendAsync",
+                Times.Between(0, 1, Moq.Range.Inclusive),
+                ItExpr.Is<HttpRequestMessage>(req => req.Method == HttpMethod.Get && req.RequestUri!.AbsoluteUri.StartsWith($"{azMapsBaseUri}/{azMapsSearchAddressQueryPath}") && req.RequestUri!.AbsoluteUri.EndsWith(expectedQueryParameters)),
+                ItExpr.IsAny<CancellationToken>()
+            );
+
+            // Check result
+            Assert.Multiple(() =>
+            {
+                Assert.That(actualResult, Is.Not.Null);
+                Assert.That(actualResult, Is.Not.Empty);
+                Assert.That(actualResult, Is.EqualTo(sampleResponse));
+            });
+        }
+        catch (Exception error)
+        {
+            Assert.Fail($"Error while testing {nameof(UnitTest)}: {error.Message}");
+        }
+    }
+
+    private static string BuildExpectedQueryParameters(IDictionary<string, string> queryParameters)
+    {
+        return string.Join('&', queryParameters.Select(kv => $"{kv.Key}={kv.Value}").ToList());
+    }
+
+    private static void BuildMocks(IIoCContainer container,
+        out IAzMapsApiToken token,
+        out Mock<IHttpClientFactory> httpClientFactoryMock,
+        out IAzMapsApiHttpRepository httpRepository
+    )
+    {
+        // Create authentication token.
+        token = container.Resolve<IAzMapsApiToken>();
+
+        // Build HTTP repository and HTTP client factory mock
+        httpClientFactoryMock = new Mock<IHttpClientFactory>(MockBehavior.Strict);
+
+        httpRepository = container.Resolve<IAzMapsApiHttpRepository>
+        (
+            IoCNamedParameter.CreateNew("httpClientFactory", httpClientFactoryMock.Object)
+        );
+    }
+
+    private const string sampleResponse = """
+                {
+            "summary": {
+                "query": "villeurbanne",
+                "queryType": "NON_NEAR",
+                "queryTime": 32,
+                "numResults": 4,
+                "offset": 0,
+                "totalResults": 4,
+                "fuzzyLevel": 1
+            },
+            "results": [
+                {
+                    "type": "Geography",
+                    "id": "ID",
+                    "score": 1,
+                    "entityType": "Municipality",
+                    "matchConfidence": {
+                        "score": 1
+                    },
+                    "address": {
+                        "municipality": "Villeurbanne",
+                        "countrySecondarySubdivision": "Rhône",
+                        "countrySubdivision": "Auvergne-Rhône-Alpes",
+                        "countrySubdivisionName": "Auvergne-Rhône-Alpes",
+                        "countrySubdivisionCode": "ARA",
+                        "countryCode": "FR",
+                        "country": "France",
+                        "countryCodeISO3": "FRA",
+                        "freeformAddress": "Villeurbanne"
+                    },
+                    "position": {
+                        "lat": 45.76467,
+                        "lon": 4.8804
+                    }
+                }
+            ]
+        }
+        """;
+}
