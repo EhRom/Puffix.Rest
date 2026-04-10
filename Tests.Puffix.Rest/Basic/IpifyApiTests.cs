0400e0b9513619ba7dbdf5788e980c1e584de3e3
ehrom <19820528+EhRom@users.noreply.github.com>
2026-04-10 19:21:07 +0200

    Add new tests

--- Tests.Puffix.Rest/Basic/IpifyApiTests.cs
+++ Tests.Puffix.Rest/Basic/IpifyApiTests.cs
@@ -108,12 +108,8 @@ public class IpifyApiTests
             string ipifyUri = (container.Configuration[nameof(ipifyUri)] ?? string.Empty).TrimEnd('/');
             IDictionary<string, string> queryParameters = new Dictionary<string, string>();
 
-            // TODO test JSON format >> ?format=json
-            //string expectedQueryParameters = BuildExpectedQueryParameters(queryParameters);
-
             IIpifyApiQueryInformation queryInformation = httpRepository.BuildAuthenticatedQuery(token, HttpMethod.Get, ipifyUri, string.Empty, queryParameters, string.Empty);
 
-            // TODO test HttpWithStatusAsync
             string actualResult = await httpRepository.HttpAsync(queryInformation);
 
             // Check calls
@@ -138,10 +134,64 @@ public class IpifyApiTests
             Assert.Fail($"Error while testing {nameof(Test)}: {error.Message}");
         }
     }
-
-    private static string BuildExpectedQueryParameters(IDictionary<string, string> queryParameters)
+    [Test]
+    public async Task UnitTestWithStatus()
     {
-        return string.Join('&', queryParameters.Select(kv => $"{kv.Key}={kv.Value}").ToList());
+        try
+        {
+            BuildMocks(container, out IIpifyApiToken token, out Mock<IHttpClientFactory> httpClientFactoryMock, out IIpifyApiBasicHttpRepository httpRepository);
+
+            // Register HTTP Calls
+            using HttpContent expectedHttpContent = new StringContent(sampleResponse ?? string.Empty, Encoding.UTF8, "text/^plain");
+
+            Mock<HttpMessageHandler> mockHttpMessageHandlerMock = new Mock<HttpMessageHandler>();
+            mockHttpMessageHandlerMock.Protected()
+                .Setup<Task<HttpResponseMessage>>("SendAsync", ItExpr.IsAny<HttpRequestMessage>(), ItExpr.IsAny<CancellationToken>())
+                .ReturnsAsync(new HttpResponseMessage
+                {
+                    StatusCode = HttpStatusCode.OK,
+                    Content = expectedHttpContent
+                });
+
+            using HttpClient httpClient = new HttpClient(mockHttpMessageHandlerMock.Object);
+
+            httpClientFactoryMock.Setup(httpClientFactory => httpClientFactory.CreateClient(It.IsAny<string>()))
+                .Returns(httpClient);
+
+            // Test
+            string ipifyUri = (container.Configuration[nameof(ipifyUri)] ?? string.Empty).TrimEnd('/');
+            IDictionary<string, string> queryParameters = new Dictionary<string, string>();
+
+            IIpifyApiQueryInformation queryInformation = httpRepository.BuildAuthenticatedQuery(token, HttpMethod.Get, ipifyUri, string.Empty, queryParameters, string.Empty);
+
+            IResultInformation<string> actualResult = await httpRepository.HttpWithStatusAsync(queryInformation);
+
+            // Check calls
+            httpClientFactoryMock.Verify(httpClientFactory => httpClientFactory.CreateClient(It.IsAny<string>()), Times.Once());
+            mockHttpMessageHandlerMock.Protected().Verify(
+                "SendAsync",
+                Times.Between(0, 1, Moq.Range.Inclusive),
+                ItExpr.Is<HttpRequestMessage>(req => req.Method == HttpMethod.Get && req.RequestUri!.AbsoluteUri.StartsWith(ipifyUri)),
+                ItExpr.IsAny<CancellationToken>()
+            );
+
+            // Check result
+            Assert.Multiple(() =>
+            {
+                Assert.That(actualResult, Is.Not.Null);
+                Assert.That(actualResult.ErrorContent, Is.Not.Null.And.Empty);
+                Assert.That(actualResult.IsSuccess, Is.True);
+                Assert.That(actualResult.ResultCode, Is.EqualTo(HttpStatusCode.OK));
+                Assert.That(actualResult.ResultContent, Is.Not.Null.And.Not.Empty);
+                Assert.That(actualResult.ResultContent, Is.EqualTo(sampleResponse));
+                Assert.That(actualResult.Headers, Is.Not.Null);
+                Assert.That(actualResult.Headers.Count, Is.EqualTo(0));
+            });
+        }
+        catch (Exception error)
+        {
+            Assert.Fail($"Error while testing {nameof(Test)}: {error.Message}");
+        }
     }
 
     private static void BuildMocks(IIoCContainer container,
